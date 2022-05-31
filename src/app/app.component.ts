44d7bdec4c0915e8e18c3eecceb483d27afa1b75
gabolano17 <gabolano60@misena.edu.co>
2022-05-31 17:38:11 -0500

    commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Balance } from './models/balance';
+import { BalanceService } from './services/balance.service';
+import { ExporterService } from './services/exporter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  balance: Balance[] = []
   title = 'Balance General';
+
+  ngOnInit(): void {
+    this.balanceService.getData().subscribe(data => this.balance = data);
+  }
+
+  constructor(private balanceService: BalanceService, private exportToExcel: ExporterService){}
+
+  descargarExcel(){
+    this.exportToExcel.exportToExcel(this.balance, 'balance');
+  }
 }
--- src/app/components/graf-barras/graf-barras.component.ts
+++ src/app/components/graf-barras/graf-barras.component.ts
@@ -6,17 +6,16 @@ import { BalanceService } from 'src/app/services/balance.service';
 @Component({
   selector: 'app-graf-barras',
   templateUrl: './graf-barras.component.html',
-  styleUrls: ['./graf-barras.component.css']
+  styleUrls: ['./graf-barras.component.css'],
 })
 export class GrafBarrasComponent implements OnInit {
-
   balance: Balance[] = [];
   grafico: any = [];
   año: any;
   costos: any;
   utilidades: any;
 
-  constructor(private service:BalanceService) { 
+  constructor(private service: BalanceService) {
     Chart.register(...registerables);
   }
 
@@ -24,43 +23,49 @@ export class GrafBarrasComponent implements OnInit {
     this.getData();
   }
 
-  getData(){
-    this.service.getData().subscribe(data => {
+  getData() {
+    this.service.getData().subscribe((data) => {
       this.balance = data;
-      this.año = this.balance.map(a => a.año);
-      this.costos = this.balance.map(c => c.costos);
-      this.utilidades = this.balance.map(u => u.utilidades);
-      this.graficar(this.año, this.costos, this.utilidades)
-    })
+      this.año = this.balance.map((a) => a.año);
+      this.costos = this.balance.map((c) => c.costos);
+      this.utilidades = this.balance.map((u) => u.utilidades);
+      this.graficar(this.año, this.costos, this.utilidades);
+    });
   }
 
-  graficar(año:any, costos:any, utilidades: any){
+  graficar(año: any, costos: any, utilidades: any) {
     this.grafico = new Chart('graficoBarra', {
       type: 'bar',
       data: {
-          labels: año,
-          datasets: [{
-              label: 'Costos',
-              data: costos,
-              backgroundColor: 'rgba(255, 99, 132, 1)',
-              borderColor: 'rgba(255, 99, 132, 1)',
-              borderWidth: 1
-          }, {
+        labels: año,
+        datasets: [
+          {
+            label: 'Costos',
+            data: costos,
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          },
+          {
             label: 'Utilidades',
             data: utilidades,
-            backgroundColor: 'rgba(75, 192, 192, 1)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
+            backgroundColor: 'rgba(54, 162, 235, 1)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
-  })
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
 
+  exportToExcel(){
+    
+  }
 }
